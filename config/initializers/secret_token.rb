17afa0687aafd7b559be46f4d3cf0e436dba573b
weyewe <weyewe@Willy-Yunnals-MacBook.local>
2011-04-09 19:29:19 +0700

    basic rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Humanresc::Application.config.secret_token = 'a6f0c74c2a2c6993863755104ab634e9fa7749facace8dac0fb738e847c107d85cda0c43b2eb8d4bf4a4c1b7ddd6e2329869848998cafab71cd197c1216faffe'

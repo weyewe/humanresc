97d46bfa8182822ad7c11ef0f448b87a09e554f5
weyewe <weyewe@Willy-Yunnals-MacBook.local>
2011-04-12 09:29:42 +0700

    add name

--- app/models/relative.rb
+++ app/models/relative.rb
@@ -1,3 +1,17 @@
 class Relative < ActiveRecord::Base
   belongs_to :employee
+  
+  
+  
+  
+  
+  def self.relationship_selections
+    relationship_selections_result = {}
+    
+    RELATIVE_CONSTANT.each do | number ,name  | 
+        relationship_selections_result[name] = number
+    end
+    
+    relationship_selections_result
+  end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Humanresc::Application.initialize!
+
+
+RELATIVE_CONSTANT = {
+  1 => "Ayah",
+  2 => "Ibu",
+  3 => "Saudara",
+  4 => "Istri/Suami (SAH)",
+  5 => "Anak",
+}
+
+SEX_CONSTANT =  {
+  0 => "Perempuan", 
+  1 => "Laki-laki"
+}
